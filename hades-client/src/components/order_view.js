3ffa6023b3ca28015c6aefcc3ae6085bb2bca429
Amogh100 <2amogh@gmail.com>
2018-02-26 23:55:28 -0700

    Code formatting

--- hades-client/src/components/order_view.js
+++ hades-client/src/components/order_view.js
@@ -3,7 +3,7 @@ import { MenuItem } from "react-bootstrap";
 import React, { Component } from "react";
 import styles from "./order_entry";
 import axios from "axios";
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
 
 class OrderView extends Component {
   constructor(props) {
@@ -16,23 +16,31 @@ class OrderView extends Component {
 
   updateOrders() {
     axios
-    .get("http://localhost:8080/order?traderId=1")
-    .then(response => this.setState({orders: response.data}));
+      .get("http://localhost:8080/order?traderId=1")
+      .then(response => this.setState({ orders: response.data }));
   }
 
   render() {
     let options = {
-        sortName: 'orderId',
-        sortOrder: 'desc'
-      }
+      sortName: "orderId",
+      sortOrder: "desc"
+    };
     return (
       <div>
         <h1>Open Orders</h1>
-        <BootstrapTable options={options} data={this.state.orders} width={5} height={5}scrollTop={'Bottom'}>
-        <TableHeaderColumn  isKey={true} dataField='orderId'>Order ID</TableHeaderColumn>
-        <TableHeaderColumn  dataField='ticker'>Ticker</TableHeaderColumn>
-        <TableHeaderColumn  dataField='price'>Price</TableHeaderColumn>
-        <TableHeaderColumn  dataField='bid'>Bid </TableHeaderColumn>
+        <BootstrapTable
+          options={options}
+          data={this.state.orders}
+          width={5}
+          height={5}
+          scrollTop={"Bottom"}
+        >
+          <TableHeaderColumn isKey={true} dataField="orderId">
+            Order ID
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField="ticker">Ticker</TableHeaderColumn>
+          <TableHeaderColumn dataField="price">Price</TableHeaderColumn>
+          <TableHeaderColumn dataField="bid">Bid </TableHeaderColumn>
         </BootstrapTable>
       </div>
     );
