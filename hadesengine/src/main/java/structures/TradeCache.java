5d0e060988043b2ce11598789ce86373294fc043
Amogh100 <Amogh.Jahagirdar@Colorado.edu>
2018-04-06 17:23:27 -0600

    Update TradeCache.java

--- hadesengine/src/main/java/structures/TradeCache.java
+++ hadesengine/src/main/java/structures/TradeCache.java
@@ -12,23 +12,37 @@ public class TradeCache {
         private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
 
+    //Constructs a TradeCache
     public TradeCache(){
         this.tradeSet = ConcurrentHashMap.newKeySet();
+        //Schedule a DB update every 100 milliseconds.
         scheduler.scheduleAtFixedRate(new TradeDatabaseUpdater(this), 100, 100, TimeUnit.MILLISECONDS);
     }
 
+    //getTrades retrieves all the Trades in the Cache
     public synchronized Trade[] getTrades() {
         return tradeSet.toArray(new Trade[tradeSet.size()]);
     }
 
+    //clearCache removes all the Trades from the Cache
     public synchronized void clearCache(){
         System.out.println("Clearing cache");
         this.tradeSet.clear();
     }
+    
+    /**
+      * Adds a Trade to the cache.
+      * @Param t Trade to add the cache.
+      */
     public synchronized void addTrade(Trade t){
         System.out.println("Added trade to cache!");
         tradeSet.add(t);
     }
+    
+    
+    /**
+      * Returns true if the cache is empty.
+      */
     public synchronized boolean isEmpty(){
         return tradeSet.isEmpty();
     }
