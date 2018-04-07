f16fd201dde9f09ef8d93c50c1e5099889907eaa
Amogh100 <Amogh.Jahagirdar@Colorado.edu>
2018-04-06 20:44:06 -0600

    Update TradeRepository.java

--- hadesrest/src/main/java/application/core/repositories/TradeRepository.java
+++ hadesrest/src/main/java/application/core/repositories/TradeRepository.java
@@ -7,9 +7,14 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+//DAO access for Trades.
 @Repository
 public interface TradeRepository extends CrudRepository<Trade,Long> {
 
+    /**
+     * findByTraderId returns all the trades where a given trader was a participant
+     * @param traderId traderId of the trader to retrieve trades for.
+     */
     @Query("SELECT t FROM Trade t WHERE t.trader1Id = ?1 OR t.trader2Id = ?1")
     List<Trade> findByTraderId(long traderId);
 
