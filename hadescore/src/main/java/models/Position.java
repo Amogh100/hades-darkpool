b1803f0c02b36520c08e492c780e543d90600f02
Amogh100 <2amogh@gmail.com>
2018-04-15 12:51:31 -0600

    Updated position model

--- hadescore/src/main/java/models/Position.java
+++ hadescore/src/main/java/models/Position.java
@@ -1,24 +1,29 @@
 package models;
 
+import javax.persistence.Entity;
 
 /**
  * Class for representing a position in a given asset
  */
+
+@Entity
 public class Position {
 
 
     private String assetId;
     private double positionSize;
-
+    private long traderId;
+    
     public String getAssetId(){
         return assetId;
     }
 
     public void updatePositionSize(double change){positionSize += change;}
 
-    public Position(String assetId, double positionSize){
+    public Position(String assetId, double positionSize, long traderId){
         this.assetId = assetId;
         this.positionSize = positionSize;
+        this.traderId = traderId;
     }
 
     public double getPositionSize() {
