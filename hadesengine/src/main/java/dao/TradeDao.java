61e00aa6b55b3b491c2cba6c970d1acb0d921670
Amogh100 <2amogh@gmail.com>
2018-04-15 22:09:04 -0600

    added data access layer

---
+++ hadesengine/src/main/java/dao/TradeDao.java
@@ -0,0 +1,30 @@
+package dao;
+
+import eventprocessing.DatabaseHelper;
+import models.entities.Trade;
+import java.lang.StringBuffer;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TradeDao {
+
+    public void insertTrade(Trade t) {
+        try {
+            Connection conn = DatabaseHelper.connect();
+            StringBuffer traderInsertionStatementStr = new StringBuffer();
+            traderInsertionStatementStr.append("INSERT INTO trade (trader1Id, trader2Id, order1Id, order2Id, price, fillSize)");
+            traderInsertionStatementStr.append(" VALUES(?, ?, ?, ?, ?, ?)");
+            PreparedStatement insertStatement = conn.prepareStatement(traderInsertionStatementStr.toString());
+            insertStatement.setLong(1, t.getTrader1Id());
+            insertStatement.setLong(2, t.getTrader2Id());
+            insertStatement.setLong(3, t.getOrder1Id());
+            insertStatement.setLong(4, t.getOrder2Id());
+            insertStatement.setDouble(5, t.getPrice());
+            insertStatement.setDouble(6, t.getSize());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
